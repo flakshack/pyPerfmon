4511fef9b2c6521197dc64963c58c1a77e3475b3
flakshack <forge@flakshack.com>
2020-03-09 11:27:17 -0400

    Update to make compatible with Python 3

--- counterid.py
+++ counterid.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """counterid - Simple utility to discover perfmon counter paths"""
-# Compile to EXE using c:\Python27\scripts\pyinstaller.exe -F counterid.py
+# pip install pyinstaller
+# Compile to EXE using pyinstaller.exe -F counterid.py
 
 __author__ = 'scottv@rbh.com (Scott Vintinner)'
 import win32pdh
@@ -8,6 +9,7 @@ import win32pdh
 
 # Will display a window with available counters.  Click add to print out counter name.
 def print_counter(counter):
-    print counter
+    print(counter)
+
 
 win32pdh.BrowseCounters(None, 0, print_counter, win32pdh.PERF_DETAIL_WIZARD, "Counter List")
